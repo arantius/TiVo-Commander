3a3eb46b03df1a5f6a32dd024cb82aa8c8d03000
Anthony Lieuallen <arantius@gmail.com>
2011-07-02 15:30:41 -0400

    Slightly more response implementation.

--- src/com/arantius/tivocommander/rpc/response/MindRpcResponse.java
+++ src/com/arantius/tivocommander/rpc/response/MindRpcResponse.java
@@ -1,18 +1,35 @@
 package com.arantius.tivocommander.rpc.response;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.util.Log;
 
 public abstract class MindRpcResponse {
-  public JSONObject mBody;
-  public Boolean mIsFinal;
-  public int mRpcId;
+  private final JSONObject mBody;
+  private final Boolean mIsFinal;
+  private final int mRpcId;
 
   public MindRpcResponse(Boolean isFinal, int rpcId, JSONObject bodyObj) {
     Log.d("mindrpcresponse", "got: " + bodyObj.toString());
     mBody = bodyObj;
     mIsFinal = isFinal;
     mRpcId = rpcId;
+  }
+
+  public Boolean isFinal() {
+    return mIsFinal;
+  }
+
+  public int getRpcId() {
+    return mRpcId;
+  }
+
+  public Object get(String key) {
+    try {
+      return mBody.get(key);
+    } catch (JSONException e) {
+      return null;
+    }
   }
 }
