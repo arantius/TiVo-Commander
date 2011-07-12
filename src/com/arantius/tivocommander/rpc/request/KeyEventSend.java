44280042fc1479f2a9145e008124a32d7625c688
Anthony Lieuallen <arantius@gmail.com>
2011-07-12 18:41:24 -0400

    Remove junk notes.

--- src/com/arantius/tivocommander/rpc/request/KeyEventSend.java
+++ src/com/arantius/tivocommander/rpc/request/KeyEventSend.java
@@ -5,12 +5,6 @@ import org.json.JSONException;
 import android.util.Log;
 
 public class KeyEventSend extends MindRpcRequest {
-  // static String[] supported = { "actionA", "actionB", "actionC", "actionD",
-  // "advance", "channelDown", "channelUp", "clear", "down", "enter",
-  // "forward", "guide", "info", "left", "liveTv", "pause", "play", "record",
-  // "replay", "reverse", "right", "select", "slow", "thumbsDown", "thumbsUp",
-  // "tivo", "up", "zoom" };
-
   public KeyEventSend(String key) {
     super("keyEventSend");
 
