2ed848b4a65cf91ecfbf92a6da9967ebd60f399d
Anthony Lieuallen <arantius@gmail.com>
2011-07-23 14:43:48 -0400

    Beginnings of content view.

--- src/com/arantius/tivocommander/Content.java
+++ src/com/arantius/tivocommander/Content.java
@@ -2,22 +2,45 @@ package com.arantius.tivocommander;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.arantius.tivocommander.rpc.MindRpc;
 import com.arantius.tivocommander.rpc.request.ContentSearch;
+import com.arantius.tivocommander.rpc.request.UiNavigate;
 import com.arantius.tivocommander.rpc.response.MindRpcResponse;
 import com.arantius.tivocommander.rpc.response.MindRpcResponseListener;
 
 public class Content extends Activity {
-  private String mContentId;
-
   private final MindRpcResponseListener contentListener =
       new MindRpcResponseListener() {
         public void onResponse(MindRpcResponse response) {
-          // todo
+          mResponse = response;
+          setContentView(R.layout.content);
         }
       };
+
+  private String mContentId;
+  private MindRpcResponse mResponse;
+
+  public void doDelete(View v) {
+    Toast.makeText(getBaseContext(), "Delete not implemented yet.",
+        Toast.LENGTH_SHORT).show();
+  }
+
+  public void doExplore(View v) {
+    Toast.makeText(getBaseContext(), "Explore not implemented yet.",
+        Toast.LENGTH_SHORT).show();
+  }
+
+  public void doWatch(View v) {
+    String recordingId =
+        mResponse.getBody().path("content").path(0)
+            .path("recordingForContentId").path(0).path("recordingId")
+            .getTextValue();
+    MindRpc.addRequest(new UiNavigate(recordingId), null);
+  }
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
--- src/com/arantius/tivocommander/rpc/request/UiNavigate.java
+++ src/com/arantius/tivocommander/rpc/request/UiNavigate.java
@@ -3,17 +3,15 @@ package com.arantius.tivocommander.rpc.request;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.codehaus.jackson.JsonNode;
-
 public class UiNavigate extends MindRpcRequest {
   /** Playback for a given item. */
-  public UiNavigate(JsonNode item) {
+  public UiNavigate(String recordingId) {
     super("uiNavigate");
 
     Map<String, Object> parameters = new HashMap<String, Object>();
     parameters.put("fUseTrioId", "true");
     parameters.put("fHideBannerOnEnter", "false");
-    parameters.put("recordingId", item.get("childRecordingId"));
+    parameters.put("recordingId", recordingId);
 
     mDataMap.put("uri", "x-tivo:classicui:playback");
     mDataMap.put("parameters", parameters);
