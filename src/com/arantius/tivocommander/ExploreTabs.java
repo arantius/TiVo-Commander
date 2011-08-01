3ccec24ace2b41f04080ecd726e7a0a6523c2e2b
Anthony Lieuallen <arantius@gmail.com>
2011-08-01 17:58:46 -0400

    Sort members.

--- src/com/arantius/tivocommander/ExploreTabs.java
+++ src/com/arantius/tivocommander/ExploreTabs.java
@@ -14,6 +14,22 @@ public class ExploreTabs extends TabActivity {
   private String mContentId;
   private TabHost mTabHost;
 
+  private TabSpec makeTab(String name, Class<? extends Activity> cls) {
+    TabSpec tab = mTabHost.newTabSpec(name);
+    tab.setIndicator(name);
+
+    Intent intent = new Intent(getBaseContext(), cls);
+    if (mCollectionId != null) {
+      intent.putExtra("collectionId", mCollectionId);
+    }
+    if (mContentId != null) {
+      intent.putExtra("contentId", mContentId);
+    }
+    tab.setContent(intent);
+
+    return tab;
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -36,21 +52,5 @@ public class ExploreTabs extends TabActivity {
     mTabHost.addTab(makeTab("Explore", Explore.class));
     mTabHost.addTab(makeTab("Credits", Credits.class));
     mTabHost.addTab(makeTab("Also", Suggestions.class));
-  }
-
-  private TabSpec makeTab(String name, Class<? extends Activity> cls) {
-    TabSpec tab = mTabHost.newTabSpec(name);
-    tab.setIndicator(name);
-
-    Intent intent = new Intent(getBaseContext(), cls);
-    if (mCollectionId != null) {
-      intent.putExtra("collectionId", mCollectionId);
-    }
-    if (mContentId != null) {
-      intent.putExtra("contentId", mContentId);
-    }
-    tab.setContent(intent);
-
-    return tab;
   }
 }
