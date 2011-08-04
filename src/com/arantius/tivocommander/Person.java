191f7e7b96ae46a5803d4397a5ff957c8abeb8c8
Anthony Lieuallen <arantius@gmail.com>
2011-08-03 23:30:15 -0400

    Simple details in Person.

--- src/com/arantius/tivocommander/Person.java
+++ src/com/arantius/tivocommander/Person.java
@@ -1,12 +1,70 @@
 package com.arantius.tivocommander;
 
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.codehaus.jackson.JsonNode;
+
 import android.app.ListActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 import com.arantius.tivocommander.rpc.MindRpc;
 import com.arantius.tivocommander.rpc.request.PersonSearch;
+import com.arantius.tivocommander.rpc.response.MindRpcResponse;
+import com.arantius.tivocommander.rpc.response.MindRpcResponseListener;
 
 public class Person extends ListActivity {
+  private final MindRpcResponseListener mPersonListener =
+      new MindRpcResponseListener() {
+        public void onResponse(MindRpcResponse response) {
+          setContentView(R.layout.list_person);
+          JsonNode person = response.getBody().path("person").path(0);
+
+          Utils.log(Utils.stringifyToPrettyJson(person));
+
+          ((TextView) findViewById(R.id.person_name)).setText(mName);
+
+          JsonNode rolesNode = person.path("roleForPersonId");
+          String[] roles = new String[rolesNode.size()];
+          for (int i = 0; i < rolesNode.size(); i++) {
+            roles[i] = rolesNode.path(i).getTextValue();
+            roles[i] =
+                roles[i].substring(0, 1).toUpperCase() + roles[i].substring(1);
+          }
+          ((TextView) findViewById(R.id.person_role)).setText(Utils.join(
+              ", ", roles));
+
+          SimpleDateFormat dateParser = new SimpleDateFormat("yyyy-mm-dd");
+          ParsePosition pp = new ParsePosition(0);
+          Date birthdate =
+              dateParser.parse(person.path("birthDate").getTextValue(), pp);
+          SimpleDateFormat dateFormatter =
+              new SimpleDateFormat("MMMMM d, yyyy");
+          Spannable birthdateStr =
+              new SpannableString("Birthdate: "
+                  + dateFormatter.format(birthdate));
+          birthdateStr.setSpan(new ForegroundColorSpan(Color.WHITE), 11,
+              birthdateStr.length(), 0);
+          ((TextView) findViewById(R.id.person_birthdate))
+              .setText(birthdateStr);
+
+          Spannable birthplaceStr =
+              new SpannableString("Birthplace: "
+                  + person.path("birthPlace").getTextValue());
+          birthplaceStr.setSpan(new ForegroundColorSpan(Color.WHITE), 12,
+              birthplaceStr.length(), 0);
+          ((TextView) findViewById(R.id.person_birthplace))
+              .setText(birthplaceStr);
+        }
+      };
+  private String mName;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -18,11 +76,12 @@ public class Person extends ListActivity {
       return;
     }
 
-//    setContentView(R.layout.progress);
     String personId = bundle.getString("personId");
-    setTitle(String.format("TiVo Commander - %s %s", bundle.getString("fName"),
-        bundle.getString("lName")));
-    MindRpc.addRequest(new PersonSearch(personId), null);
+    mName =
+        String.format("%s %s", bundle.getString("fName"),
+            bundle.getString("lName"));
+    setTitle("TiVo Commander - " + mName);
+    MindRpc.addRequest(new PersonSearch(personId), mPersonListener);
   }
 
   @Override
