5645b7d59c3575b09dd24de4ac8363b3d139596a
Anthony Lieuallen <arantius@gmail.com>
2011-08-06 17:17:10 -0400

    Add version, JmDNS disclaimer to About.

--- src/com/arantius/tivocommander/About.java
+++ src/com/arantius/tivocommander/About.java
@@ -1,12 +1,25 @@
 package com.arantius.tivocommander;
 
 import android.app.Activity;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class About extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.about);
+
+    String version = " v";
+    try {
+      version +=
+          getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+    } catch (NameNotFoundException e) {
+      version = "";
+    }
+
+    TextView title = (TextView) findViewById(R.id.textView1);
+    title.setText(title.getText() + version);
   }
 }
