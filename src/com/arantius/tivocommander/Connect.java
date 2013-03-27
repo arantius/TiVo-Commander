b8d8d3bb3530ddf9fd328ce853f7e5a73552bcfe
Anthony Lieuallen <arantius@gmail.com>
2013-03-27 19:19:19 -0400

    Support for beta builds.

--- src/com/arantius/tivocommander/Connect.java
+++ src/com/arantius/tivocommander/Connect.java
@@ -19,12 +19,20 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 package com.arantius.tivocommander;
 
+import java.util.Date;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.arantius.tivocommander.rpc.MindRpc;
 
 public class Connect extends Activity {
+//  final private static Date BETA_EXPIRATION_TIME = new Date(113, 0, 1);
+  final private static Date BETA_EXPIRATION_TIME = null;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -42,6 +50,15 @@ public class Connect extends Activity {
   protected void onResume() {
     super.onResume();
     Utils.log("Activity:Resume:Connect");
+
+    if (BETA_EXPIRATION_TIME != null && new Date().after(BETA_EXPIRATION_TIME)) {
+      Toast.makeText(this, "This beta has expired, please update the app.",
+          Toast.LENGTH_LONG).show();
+      Intent intent = new Intent(Intent.ACTION_VIEW);
+      intent.setData(Uri.parse("market://details?id=com.arantius.tivocommander"));
+      startActivity(intent);
+      finish();
+    }
 
     // Start on a separate thread so this UI shows immediately.
     new Thread(new Runnable() {
