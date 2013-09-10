02788d096739e4a5a79e2cfb3959c187642d1f83
Anthony Lieuallen <arantius@gmail.com>
2013-09-10 18:25:27 -0400

    Initialize Device properties.
    Setting port fixes a crash when adding a custom device.

--- src/com/arantius/tivocommander/Device.java
+++ src/com/arantius/tivocommander/Device.java
@@ -1,10 +1,10 @@
 package com.arantius.tivocommander;
 
 public class Device {
-  public String addr;
-  public String device_name;
+  public String addr = null;
+  public String device_name = null;
   public Long id = null;
-  public String mak;
-  public Integer port;
+  public String mak = null;
+  public Integer port = 1413;
   public String tsn = "-";
 }
