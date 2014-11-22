cbb3a6b45f20024469aadbe87db6f2bb6773b966
Anthony Lieuallen <arantius@gmail.com>
2014-11-22 16:01:53 -0500

    Subscription options: 30/60/90 minutes late.

--- src/com/arantius/tivocommander/SubscribeBase.java
+++ src/com/arantius/tivocommander/SubscribeBase.java
@@ -12,12 +12,14 @@ import com.arantius.tivocommander.rpc.request.Subscribe;
 abstract public class SubscribeBase extends Activity {
   protected final static String[] mStartLabels = new String[] { "On time",
       "1 minute early", "2 minutes early", "3 minutes early",
-      "4 minutes early", "5 minutes early", "10 minutes early" };
+      "4 minutes early", "5 minutes early", "10 minutes early"};
   protected final static Integer[] mStartStopValues = new Integer[] { 0,
-      60 * 1, 60 * 2, 60 * 3, 60 * 4, 60 * 5, 60 * 10 };
-  protected final static String[] mStopLabels = new String[] { "On time",
+      60 * 1, 60 * 2, 60 * 3, 60 * 4, 60 * 5, 60 * 10, 60 * 30, 60 * 60,
+      60 * 90 };
+  protected final static String[] mStopLabels = new String[] {"On time",
       "1 minute late", "2 minutes late", "3 minutes late", "4 minutes late",
-      "5 minutes late", "10 minutes late" };
+      "5 minutes late", "10 minutes late", "30 minutes late",
+      "60 minutes late", "90 minutes late"};
   protected final static String[] mUntilLabels = new String[] { "Space needed",
       "Until I delete" };
   protected final static String[] mUntilValues = new String[] { "fifo",
